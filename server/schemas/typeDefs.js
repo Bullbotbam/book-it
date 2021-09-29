594e2c2c529d94c46d18c498547dfb1f61549b2a
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-09-29 18:58:43 -0500

    added schemas, typedefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    books: [book]
+}
+
+type Book {
+    _id: ID 
+    createdAt: String
+    author: String
+    title: String
+    description: String
+    link: String
+    image: String
+
+}
+
+type Query {
+    me: User 
+    users: [User]
+    user(username: String!): User 
+    thoughts(username: String): [Book]
+    book(_id: ID!): Book 
+}
+
+type Mutation {
+    login(email: String!, password; String!): Auth 
+    addUser(username: String!, email: String!, password: String!): Auth 
+    addBook(title: String!): Book
+}
+
+`;
