13c0eea7cf6af12a9147b96dbcdce709ef6cb837
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-01 12:23:21 -0500

    isses with authentication need to be addressed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,40 +2,38 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    bookCount: Int
-    books: [book]
-}
+	type User {
+		_id: ID
+		username: String
+		email: String
+		password: String
+		bookCount: Int
+		books: [book]
+	}
 
-type Book {
-    _id: ID 
-    createdAt: String
-    author: String
-    title: String
-    description: String
-    link: String
-    image: String
+	type Book {
+		_id: ID
+		createdAt: String
+		author: String
+		title: String
+		description: String
+		link: String
+		image: String
+	}
 
-}
+	type Query {
+		me: User
+		users: [User]
+		user(username: String!): User
+		books(title: String): [Book]
+		book(_id: ID!): Book
+	}
 
-type Query {
-    me: User 
-    users: [User]
-    user(username: String!): User 
-    books(title: String): [Book]
-    book(_id: ID!): Book 
-}
-
-type Mutation {
-    login(email: String!, password; String!): Auth 
-    addUser(username: String!, email: String!, password: String!): Auth 
-    addBook(title: String!): Book
-}
-
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		addBook(title: String!): Book
+	}
 `;
 
 // export typeDefs;
